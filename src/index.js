3c04b43f01d9e24e34cabd92996ff6b1515e1f29
mikhaelHan <hancharuk270185@mail.ru>
2022-02-09 16:50:48 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let mass = n < 0 ? Array.from(String(n * (-1)), Number) : Array.from(String(n), Number);
+	let newMass = '';
+	for (let i = (mass.length - 1); i >= 0; i--) {
+		newMass += mass[i]
+	}
+	return Number(newMass)
 }
